ae32d0160e54afc6a77bd7478fdd2f111d8885d0
Richard <54506990+RichLarIva@users.noreply.github.com>
2024-02-20 13:55:27 +0100

    Basic skeleton for the project

---
+++ src/main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Lets play a game of "Pig (dice game)".
+
+Will throw a singular dice between 1 and 6.
+
+"""
+
+import shell
+
+
+def main():
+    """Executes the main program."""
+    print(__doc__)
+    shell.Shell.cmdloop()
+
+
+if __name__ == "__main__":
+    main()
