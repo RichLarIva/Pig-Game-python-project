c89aaebc243a1092bc4a74d1f0712efd7b955f4b
Richard <54506990+RichLarIva@users.noreply.github.com>
2024-02-21 14:44:07 +0100

    Update intelligence.py

--- src/intelligence.py
+++ src/intelligence.py
@@ -12,6 +12,7 @@ class Intelligence:
     def __init__(self):
         correct_format = False
         self.die = dice.Dice()
+        self.score = 0
         while correct_format == False:
             ai_level = input("What difficulty do you want?\n1 or 2: ")
             if ai_level == "1" or ai_level == "2":
@@ -25,7 +26,8 @@ class Intelligence:
             i = 0
             if i == 0:
                 """Throw dice"""
-                die.throw()
+                self.die.throw()
+                print(f"AI rolled {self.die.current_number}")
                 if self.die.current_number != 1:
                     self.score += self.die.current_number
                     i += 1
@@ -37,13 +39,17 @@ class Intelligence:
                 choice = random.randint(1, 2)
                 # if choice is 1 then continue throwing else just switch turn
                 if choice == 2:
+                    print(f"AI scored {self.score}")
+                    is_turn = False
                     break
-                die.throw()
+                self.die.throw()
+                print(f"AI rolled {self.die.current_number}")
                 if self.die.current_number != 1:
                     self.score += self.die.current_number
                     i += 1
                 else:
                     is_turn = False
+                    print(f"AI scored {self.score}")
                     break
                 
 
